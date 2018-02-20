d75d9923f2370d14a79a1ba3c404b7db1fb52fa9
Edgar <edgano@gmail.com>
2018-02-20 11:32:56 +0100

    Add files via upload

---
+++ bin/populate_fasta.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import sys
+from Bio import SeqIO
+
+fasta_file = sys.argv[1]  # Input fasta file
+number_file = sys.argv[2] # Input interesting numbers file, one per line
+result_file = sys.argv[3] # Output fasta file
+
+wanted = set()
+with open(number_file) as f:
+    for line in f:
+        line = line.strip()
+        if line != "":
+            wanted.add(line)
+fasta_sequences = SeqIO.parse(open(fasta_file),'fasta')
+end = False
+with open(result_file, "w") as f:
+    for seq in fasta_sequences:
+        if seq.id in wanted:
+            SeqIO.write([seq], f, "fasta")
